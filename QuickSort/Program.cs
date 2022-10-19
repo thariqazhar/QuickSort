fb3b3aab840b84f0ee444550a8a9e76ecfc54b98
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-19 17:06:30 +0700

    Swapping element at index x with index y

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -41,5 +41,14 @@ namespace QuickSort
                 arr[i] = Int32.Parse(s1);
             }
         }
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
     }
 }
