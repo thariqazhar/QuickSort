3d39ebfaeada4c5c677a9ba9a13109d93e80ec46
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-19 17:25:41 +0700

    partitioning the list into two parts

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -55,6 +55,34 @@ namespace QuickSort
             int pivot, i, j;
             if (low > high)
                 return;
+
+            //partition the list into two parts:
+            //one containing elements of less that or equal to pivot
+            //outher containing elements greater than pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greater than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+            }
         }
     }
 }
