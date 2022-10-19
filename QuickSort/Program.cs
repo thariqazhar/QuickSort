841b9fcf095381b6de12aef43a5ef76e2ce5a592
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-19 17:44:56 +0700

    add method for displaying the sorted array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -104,5 +104,18 @@ namespace QuickSort
             //sort the list on the right of pivot using quick sort
             q_sort(j+1, high);
         }
+        void display()
+        {
+            Console.Writeline("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("\n---------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
     }
 }
