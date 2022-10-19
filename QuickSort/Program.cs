92727781b845fecb965b5dc9c837550eb6255f89
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-19 16:46:47 +0700

    Declare array, comparison count, and movement count

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -8,9 +8,17 @@ namespace QuickSort
 {
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison
+        private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
 
         static void Main(string[] args)
         {
+
         }
     }
 }
