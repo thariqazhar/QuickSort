18a0f9bb89bd39d71ae738930a9c1ac34bccf6b8
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-19 16:54:56 +0700

    add method for entering number in the array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -16,9 +16,19 @@ namespace QuickSort
         //number of elements in array
         private int n;
 
-        static void Main(string[] args)
-        {
 
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
+            }
         }
     }
 }
